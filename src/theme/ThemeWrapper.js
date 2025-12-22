af19b4c02a5e0ba99b5ba2acce4a0f459cf2ea55
Ivo Patty [CTS] <ivo.patty@cts.co>
2025-12-22 19:49:33 +0100

    feat(cache) Add TSS cache

--- src/theme/ThemeWrapper.js
+++ src/theme/ThemeWrapper.js
@@ -2,12 +2,21 @@ import React from 'react';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import theme from './theme';
+import { CacheProvider } from '@emotion/react';
+import createCache from '@emotion/cache';
+
+const muiCache = createCache({
+  key: 'mui',
+  prepend: true,
+});
 
 export const ThemeWrapper = ({ children }) => (
-  <ThemeProvider theme={theme}>
-    <CssBaseline />
-    {children}
-  </ThemeProvider>
+  <CacheProvider value={muiCache}>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      {children}
+    </ThemeProvider>
+  </CacheProvider>
 );
 
 export default ThemeWrapper;
